20a604fc129a272b227219da038d7402fe36d90d
niftus13 <niftus13@gmail.com>
2023-06-29 11:33:02 +0900

    0629

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import Kiosk from "./components/Kiosk";
+import ZCounter from "./components/zcounter";
 
 
 function App() {
   return (
-    <div className="border-spacing-8 font-bold">
-      <Kiosk></Kiosk>
+    // <div className="border-spacing-8 font-bold">
+    //   <Kiosk></Kiosk>
+    // </div>
+    <div>
+      <ZCounter/>
     </div>
   );
 }
