d90425a7280595d580af53b5e461aff6226b08e7
niftus13 <niftus13@gmail.com>
2023-06-29 16:22:11 +0900

    0629

--- src/App.js
+++ src/App.js
@@ -3,20 +3,14 @@ import ZKiosk from "./components/zKiosk";
 import ZCounter from "./components/zcounter";
 import SampleLayout from "./layout/SampleLayout";
 import TemplatePage from "./layout/TemplatePage";
+import TodoInput from "./todo/TodoInput";
 
 
 function App() {
   return (
-    // <div className="border-spacing-8 font-bold">
-    //   <Kiosk></Kiosk>
-    // </div>
+
     <div>
-      <TemplatePage>
-        <div className="text-6xl text-red-800">LOGO HEADER</div>
-        <ZKiosk/>
-        <div className="text-6xl text-red-800">LOGO FOOTER</div>
-      </TemplatePage>
- 
+      <TodoInput></TodoInput>
     </div>
   );
 }
