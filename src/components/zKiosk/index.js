91139572ba6f7dae8190664ab9b253082af5755c
niftus13 <niftus13@gmail.com>
2023-06-29 14:01:02 +0900

    Zkiosk-0629 ing

--- src/components/zKiosk/index.js
+++ src/components/zKiosk/index.js
@@ -9,22 +9,46 @@ import ZCart from "./ZCart"
 
 const ZKiosk = () => {
 
+    // 1-7 cart에 뿌려주기
+    const [items,setItems]=useState([])
+
+
     //1-5 logic
     // 상품목록에서 필요한 함수
-    const buyProduct = () =>{
-        console.log("buyProduct")
+    //1-11 setItems cart로 보내주기
+    const buyProduct = (product) =>{
+        console.log("buyProduct",product)
+        // 해당상품이 카트에 있는지?
+        //1-12 filter
+        const targetArr = items.filter(item => item.pno === product.pno)
+
+        if(targetArr.length === 0) {
+            setItems([...items, {...product, qty:1}])
+            return
+        }
+        targetArr[0].qty += 1
+        setItems([...items])
     }
 
     const viewProduct = () =>{
         console.log("viewProduct")
     }
 
-    // 1-7 cart
-    const [items,setItems]=useState([])
 
     // 1-8 수량 바꿔주는 함수 -> cart
-    const changeQty = () => {
+    const changeQty = (pno, amount) => {
         console.log("changeQty")
+
+        const targetItem = items.filter(item => item.pno === pno)[0]
+        targetItem.qty += amount
+
+        if(targetItem.qty === 0 ){
+            setItems(items.filter(item => item.pno !== pno))
+            return
+        }
+        
+        setItems([...items])
+
     }
 
 
